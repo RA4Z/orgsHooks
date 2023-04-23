b262a1f2faa03f8a87e878d86f372182a9cbd895
RA4Z <rraz639@gmail.com>
2023-04-23 10:02:48 -0300

    Otimização de edição para o usuário

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,13 +1,24 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer, useMemo } from "react";
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import Estrelas from "../../../components/Estrelas";
+
+const distanciaEmMetros = (distancia) => {
+    console.log('distanciaEmMetros')
+    return `${distancia}m`
+}
 
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
     const [ selecionado, inverterSelecionado ] = useReducer(
         (selecionado) => !selecionado,
         false  
     );
+
+    const distanciaTexto = useMemo(
+        () => distanciaEmMetros(distancia), 
+        [distancia]
+    );
+
     return (
         <TouchableOpacity 
             style={estilos.cartao}
@@ -23,7 +34,7 @@ export default function Produtor({ nome, imagem, distancia, estrelas }) {
                         grande={selecionado}
                     />
                 </View>
-                <Text style={estilos.distancia}>{ distancia }</Text>
+                <Text style={estilos.distancia}>{ distanciaTexto }</Text>
             </View>
         </TouchableOpacity>
     )
